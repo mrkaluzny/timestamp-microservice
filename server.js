f51c4530f49d78bf3f9d100253b983adbdf25f09
Wojciech Kałużny <wojciech.kaluzny@me.com>
2016-09-18 22:00:01 +0000

    Minor improvements

--- server.js
+++ server.js
@@ -14,24 +14,19 @@ app.get('/:query', function(req, res){
     var myDate;
     if(/^\d{8,}$/.test(req.params.query)) {
         myDate = moment(req.params.query, "X");
-        
     } else {
         myDate = moment(req.params.query, "MMMM D, YYYY");
-        
     }
     
     if(myDate.isValid()) {
         res.json({
             unix: myDate.format("X"),
             natural: myDate.format("MMMM D, YYYY")
-            
         });
-        
     } else {
         res.json({
             unix: null,
             natural: null
-            
         });
     }
 });
